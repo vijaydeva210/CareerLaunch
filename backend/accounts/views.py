bb7858ee08cfa200c5d6730bccb1aceedda834ff
Vijay Puligilla <vijaypuligilla58@gmail.com>
2026-06-13 17:26:45 +0530

    Completed creating django admin panel, StudentProfile and testing StudentProfile

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializers
 
-# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self,request):
+        serializer = RegisterSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Student registered successfully!"},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
