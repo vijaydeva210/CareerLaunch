e5880f623c46d76aebb56268174554be6f4000f2
Vijay Puligilla <vijaypuligilla58@gmail.com>
2026-06-13 13:58:57 +0530

    completed stetting up the base architecture,accounts and assessment apps

---
+++ backend/accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class StudentProfile(models.Model):
+    #Django built-in user table
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    #Student custom rows
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    college = models.CharField(max_length=250, blank=True, null=True)
+    branch = models.CharField(max_length=150, blank=True, null=True)
+    graduation_year = models.IntegerField(blank=True, null=True)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
